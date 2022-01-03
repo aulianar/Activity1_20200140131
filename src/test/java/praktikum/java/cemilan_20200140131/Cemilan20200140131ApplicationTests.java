2b1217d81d5f9968fc5d211793262a9414ba79df
aulianar <arisky540@gmail.com>
2022-01-04 03:42:03 +0700

    act cemilan

---
+++ src/test/java/praktikum/java/cemilan_20200140131/Cemilan20200140131ApplicationTests.java
@@ -0,0 +1,13 @@
+package praktikum.java.cemilan_20200140131;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cemilan20200140131ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
